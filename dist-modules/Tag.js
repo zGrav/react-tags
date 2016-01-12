dcf7624d7ce7f74638e781a81be3e9a456765a23
David Silva <zgrav@null.net>
2016-01-12 15:03:46 +0000

    Update Tag.js

--- dist-modules/Tag.js
+++ dist-modules/Tag.js
@@ -4,40 +4,11 @@ var React = require('react');
 
 var _require = require('react-dnd');
 
-var DragSource = _require.DragSource;
-var DropTarget = _require.DropTarget;
-
 var flow = require('lodash/function/flow');
 
 var ItemTypes = { TAG: 'tag' };
 
-var tagSource = {
-    beginDrag: function beginDrag(props) {
-        return { id: props.tag.id };
-    }
 };
-
-var tagTarget = {
-    hover: function hover(props, monitor) {
-        var draggedId = monitor.getItem().id;
-        if (draggedId !== props.id) {
-            props.moveTag(draggedId, props.tag.id);
-        }
-    }
-};
-
-function dragCollect(connect, monitor) {
-    return {
-        connectDragSource: connect.dragSource(),
-        isDragging: monitor.isDragging()
-    };
-}
-
-function dropCollect(connect, monitor) {
-    return {
-        connectDropTarget: connect.dropTarget()
-    };
-}
 
 var Tag = React.createClass({
     displayName: 'Tag',
@@ -46,7 +17,6 @@ var Tag = React.createClass({
         labelField: React.PropTypes.string,
         onDelete: React.PropTypes.func.isRequired,
         tag: React.PropTypes.object.isRequired,
-        moveTag: React.PropTypes.func.isRequired
     },
     getDefaultProps: function getDefaultProps() {
         return {
@@ -56,14 +26,10 @@ var Tag = React.createClass({
     render: function render() {
         var label = this.props.tag[this.props.labelField];
         var _props = this.props;
-        var connectDragSource = _props.connectDragSource;
-        var isDragging = _props.isDragging;
-        var connectDropTarget = _props.connectDropTarget;
 
-        return connectDragSource(connectDropTarget(React.createElement(
+        return React.createElement(
             'span',
-            { style: { opacity: isDragging ? 0 : 1 },
-                className: 'ReactTags__tag' },
+            { className: 'ReactTags__tag' },
             label,
             React.createElement(
                 'a',
@@ -71,7 +37,7 @@ var Tag = React.createClass({
                     onClick: this.props.onDelete },
                 'x'
             )
-        )));
+        );
     }
 });
 
